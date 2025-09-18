fcaa44989cd787b1537f2184a3423b64779872e0
Nima <nima010607@gmail.com>
2025-09-18 10:13:54 +0200

    IfileService and FileResult created

---
+++ infrastructure/Interface/IFileService.cs
@@ -0,0 +1,12 @@
+﻿
+
+using infrastructure.Models;
+
+namespace infrastructure.Interface;
+
+public interface IFileService
+{
+    //behöver sök väg till filen, behöver innehåll där den böver sparas in då bool tells om de har sparts elr inte
+    FileResult SaveContentToFile(string path, string content);
+    FileResult GetContentFormFile(string path);
+}
---
+++ infrastructure/Models/ProductResult.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace infrastructure.Models;
+
+public class ProductResult
+{
+    public bool Succeeded { get; set; }
+    public string? Error { get; set; }
+
+}
+public class ProductResult<T> : ProductResult
+{
+
+    public T? Result { get; set; }
+}
